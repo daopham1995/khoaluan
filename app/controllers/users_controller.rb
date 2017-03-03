70e74b735f203b4ef64e4e1cdc8d04b3716ce499
daopham195 <daopt22@gmail.com>
2017-03-03 22:44:01 +0700

    daopham

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,22 @@ class UsersController < ApplicationController
   def index
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+  end
+
   def show
   end
 
   def edit
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :email, :password
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'users/edit'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 end
